a2eccc0333b2a543a4a171f37d4194fd31cd2b99
LAPTOP-A83LG6TQ\User <tauqeerulhassan45@gmail.com>
2026-05-09 10:31:06 +0100

    Student added in clean architecture

---
+++ src/SchoolManagement.Domain/Entities/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagement.Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Gender { get; set; } = string.Empty;
+    }
+}
